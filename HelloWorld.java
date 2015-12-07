c3c3def19e071badd24670c6c89c7a5849bac4e3
mblackhall <m.blackhall@upcmail.nl>
2015-12-07 10:39:11 +0100

    Hello World project

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+
+       public static void main( String args [] ) {
+
+            if ( args == null || args.length == 0 ){
+
+              System.out.println("Must supply an argument" );
+              System.exit(0);
+            }
+
+            System.out.println("Hello " + args[0]);
+       }
+
+
+}
